307c634b2396321361dea4f144b79e1f00d5bd0e
Daniel Beardsley <daniel@ifixit.com>
2013-03-17 23:11:39 -0700

    Add services list

--- status.js
+++ status.js
@@ -1,16 +1,26 @@
 function loadNotices() {
    $.getJSON('notices.json', function(data) {
-      var noticeElements = [];
-      console.log(data);
-      $.each(data.notices, function(index, notice) {
-         console.log(notice);
+      var noticeElements = $.map(data.notices, function(notice) {
          var element = $('<li>');
          element.addClass('notice');
          element.text(notice.date + " -- " + notice.note);
-         // Do something with services state
-         noticeElements.push(element);
+         return element;
       });
-      $('#notices').empty().append(noticeElements);
+
+      var serviceElements = $.map(data.services, function(available, service) {
+         var element = $('<li>');
+         var state = available === true  ? 'available' :
+                     available === false ? 'unavailable' :
+                                           'partial';
+         var status = state === 'partial' ?  available : state;
+         element.addClass('service');
+         element.addClass(state);
+         element.text(service + " -- " + status);
+         return element;
+      });
+
+      $('#notices' ).empty().append(noticeElements);
+      $('#services').empty().append(serviceElements);
       setTimeout(loadNotices, 30000);
    });
 }
